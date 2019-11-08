49ff330e3b3781dc54c8a1304a145854db90ba70
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-08 23:49:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,47 @@
-# Add  code here!
+  
+def prime?(value)
+  if value < 2
+    return false 
+  elsif 
+  (2..value -1).each do |i|
+    value % i == 0
+  if return false
+  end 
+  end
+  end 
+true 
+end 
+
+
+
+
+
+
+
+
+
+
+# def prime?(i)
+#   return false if i < 2
+#   (2..i -1).each do |x|
+#     if i % x == 0 
+#       return false 
+#     end 
+#   end 
+#   true 
+# end 
+  
+  
+  
+#   def prime?(value)
+#   if value <= 1 || value == 0 || value == 1
+#     return false
+#   elsif
+#     (2..value - 1).each do |i|
+#       if value % i == 0
+#         return false
+#       end
+#     end
+#   end
+#   true  # <= need explanation
+# end
